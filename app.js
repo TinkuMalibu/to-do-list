1225674ca250a88fc09a7f7382312cd45b8d1a1e
cecilia reynoso <cecireynoso@gmail.com>
2023-10-31 10:31:48 -0300

    To Do list challenge

---
+++ app.js
@@ -0,0 +1,8 @@
+const input = document.getElementById('to-do-task');
+const button = document.querySelector('button');
+const list = document.getElementById('task-list');
+
+// what happens when the button is clicked
+// what happens when the task is completed
+// what happens when the task is deleted
+// is the user clicks enter adds a task: 'keydown' event
