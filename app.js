cb87309e8c145646437dba2dc3dd0aae78c64f68
cecilia reynoso <cecireynoso@gmail.com>
2023-11-02 15:10:11 -0300

    to do list challenge

--- app.js
+++ app.js
@@ -19,9 +19,11 @@ let agregarTarea = () => {
 
     let completar = document.createElement('i');
     completar.classList.add('bi', 'bi-check-square-fill', 'icono-completar');
+    completar.addEventListener('click', tareaCompletada);
 
     let eliminar = document.createElement('i');
     eliminar.classList.add('bi','bi-trash3-fill', 'icono-eliminar');
+    eliminar.addEventListener('click', eliminarTarea);
 
     iconos.append(completar, eliminar);
 
@@ -32,9 +34,14 @@ let agregarTarea = () => {
 
 }
 
-let tareaCompletada = (tarea) => {
+let tareaCompletada = (e) => { // e es objeto de evento y hay que obtener el elemento que disparo el evento
+  let tarea = e.target.parentNode.parentNode;
  tarea.classList.toggle('completada');
 }
 
+let eliminarTarea = (e) => {
+  let tarea = e.target.parentNode.parentNode;
+  tarea.remove();
+}
 
 boton.addEventListener('click', agregarTarea);
